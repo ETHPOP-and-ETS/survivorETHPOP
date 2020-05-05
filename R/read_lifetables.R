4feda84f2ba343b74d81ab4336da91ed15429dfd
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2020-05-05 10:09:54 +0100

    Include all lifetable data in repo inst/extdata folder

--- R/read_lifetables.R
+++ R/read_lifetables.R
@@ -1,22 +1,43 @@
 
-#' read_life tables
+#' read ONS life tables
 #'
-#' @param input_dir
-#' @param output_dir
+#' lifetable extracted from: lttemplateew2018.xls
+#' https://www.ons.gov.uk/peoplepopulationandcommunity/birthsdeathsandmarriages/
+#' lifeexpectancies/datasets/nationallifetablesunitedkingdomreferencetables
+#'
+#' @inherit purrr
+#' @param input_dir read data folder
+#' @param output_dir save data
 #'
 #' @return
 #' @export
 #'
-#' @examples
-#'
-read_lifetables <- function(input_dir = here::here("raw data"),
+read_lifetables <- function(input_dir =
+                              system.file("extdata", "lttemplateew_single_years", package = "survivorETHPOP"),
                             output_dir = "") {
 
   dir_names  <- dir(input_dir, full.names = TRUE)
-  file_names <- dir_names[grepl("lifetable", dir_names)]
+  file_names <- dir_names[grepl(".xlsx", dir_names)]
 
-  dat <- map(file_names, read_csv, progress = FALSE)
+  dat <- map(file_names,
+             .f = read_lt)
+
   names(dat) <- map(dat, ~.$year[1])
 
   return(dat)
+}
+
+#
+read_lt <- function(file_name) {
+
+  ltm <- readxl::read_xlsx(file_name, range = "A9:G110")
+  ltf <- readxl::read_xlsx(file_name, range = "I9:O110")
+
+  ltm$sex = "M"
+  ltf$sex = "F"
+
+  ltm <- ltm %>% rename(age = `Age x`, year = Year)
+  ltf <- ltf %>% rename(age = `Age x`, year = Year)
+
+  rbind.data.frame(ltm, ltf)
 }
